64dcfa3c2ee51abcccf1cd1fdc774359f5976b98
Leonardo Nascimento Cintra <leonardo.ncintra@outlook.com>
2015-09-21 22:27:09 -0300

    Add folder templates/mail

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask.ext.login import login_user
+from flask.ext.login import login_user, login_required
 from . import auth
 from ..models import User
 from .forms import LoginForm
@@ -15,3 +15,10 @@ def login():
 			return redirec(request.args.get('next') or url_for('main.index'))
 		flash('Usuaário ou senha inválidos')
 	return render_template('auth/login.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	flash('Você saiu com sucesso!')
+	return redirect(url_for('main.index'))
