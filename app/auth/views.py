fbbc038ab814e3b6314a425aa8de122e59f0dce0
Leonardo Nascimento Cintra <leonardo.ncintra@outlook.com>
2015-09-22 21:51:41 -0300

    CSRF Token = False

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, request, url_for, flash
-from flask.ext.login import login_user, login_required
+from flask.ext.login import login_user, logout_user, login_required
 from . import auth
 from ..models import User
 from .forms import LoginForm
@@ -12,7 +12,7 @@ def login():
 		user = User.query.filter_by(email=form.email.data).first()
 		if user is not None and user.verify_password(form.password.data):
 			login_user(user, form.remember_me.data)
-			return redirec(request.args.get('next') or url_for('main.index'))
+			return redirect(request.args.get('next') or url_for('main.index'))
 		flash('Usuaário ou senha inválidos')
 	return render_template('auth/login.html', form=form)
 
